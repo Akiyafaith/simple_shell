b0c7c8f7eb7b2c1a550c70634571974fe739cafb
Akiyafaith <akiyafaith@gmail.com>
2023-02-22 15:07:49 -0800

    getline

--- getline.c
+++ getline.c
@@ -126,7 +126,7 @@ int _getline(info_t *info, char **ptr, size_t *length)
 		s = *length;
 	if (i == len)
 		i = len = 0;
-	r = read_buf (info, buf, &len);
+	r = read_buf(info, buf, &len);
 	if (r == -1 || (r == 0 && len == 0))
 		return (-1);
 
