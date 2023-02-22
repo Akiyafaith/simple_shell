a0ea68618bd11e0cf38d4faad5a360899af366f0
Akiyafaith <akiyafaith@gmail.com>
2023-02-22 08:15:41 -0800

    lists

--- lists.c
+++ lists.c
@@ -32,7 +32,6 @@ list_t *add_node(list_t **head, const char *str, int num)
 	*head = new_head;
 	return (new_head);
 }
-
 /**
  * add_node_end - adds a node to the end of the list
  * @head: address of pointer to head node
@@ -46,6 +45,7 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 
 	if (!head)
 		return (NULL);
+
 	node = *head;
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
@@ -61,7 +61,6 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 			return (NULL);
 		}
 	}
-}
 if (node)
 {
 	while (node->next)
@@ -72,7 +71,25 @@ else
 	*head = new_node;
 return (new_node);
 }
+/**
+ * print_list_str - prints only the str element of a list_t linked list
+ * @h: pointer to first node
+ *
+ * Return: size of list
+ */
+size_t print_list_str(const list_t *h)
+{
+	size_t i = 0;
 
+	while (h)
+	{
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
 /**
  * delete_node_at_index - deletes node at given index
  * @head: address of pointer to first node
@@ -82,7 +99,6 @@ return (new_node);
 int delete_node_at_index(list_t **head, unsigned int index)
 {
 	list_t *node, *prev_node;
-
 	unsigned int i = 0;
 
 	if (!head || !*head)
@@ -92,6 +108,43 @@ int delete_node_at_index(list_t **head, unsigned int index)
 	{
 		node = *head;
 		*head = (*head)->next;
+		free(node->str);
+		free(node);
+		return (1);
+	}
+	node = *head;
+	while (node)
+	{
+		if (i == index)
+		{
+			prev_node->next = node->next;
+			free(node->str);
+			free(node);
+			return (1);
+		}
+		i++;
+		prev_node = node;
+		node = node->next;
+	}
+	return (0);
+}
+/**
+ * free_list - frees all nodes of a list
+ * @head_ptr: address of pointer to head node
+ *
+ * Return: void
+ */
+void free_list(list_t **head_ptr)
+{
+	list_t *node, *next_node, *head;
+
+	if (!head_ptr || !*head_ptr)
+		return;
+	head = *head_ptr;
+	node = head;
+	while (node)
+	{
+		next_node = node->next;
 		free(node->str);
 		free(node);
 		node = next_node;
